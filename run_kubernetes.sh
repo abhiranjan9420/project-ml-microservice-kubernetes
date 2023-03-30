fdee09649751c020a42244fc85d7422c6b09deba
abhiranjan9420 <37441159+abhiranjan9420@users.noreply.github.com>
2023-03-30 22:47:21 +0530

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,7 +5,7 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-dockerpath="anubhavmilind/udacitypr4"
+dockerpath="abhiranjan88/udacity-04"
 # Step 2
 # Run the Docker Hub container with kubernetes
 kubectl run udacitypr4 \
